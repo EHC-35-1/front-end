419b959d78f98efc7cecc675367ab046e65ac953
baotan1909 <baotan1909@gmail.com>
2024-01-27 17:06:01 +0700

    Test dummy HTML

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -1,5 +1,42 @@
 function Project() {
-    return <h1>Project Page</h1>;
+  return (
+    <>
+      <h1>Project Page</h1>
+
+      <h2>Cryptocurrency</h2>
+      <p>
+        <strong>What is it?</strong> Cryptocurrency is a digital or virtual form
+        of currency that uses cryptography for security and operates on a
+        decentralized network called blockchain.
+      </p>
+      <p>
+        <strong>How it works?</strong> Cryptocurrencies work on a technology
+        called blockchain, which is a distributed ledger that records all
+        transactions across a network of computers. It uses cryptography to
+        secure transactions and control the creation of new units.
+      </p>
+      <p>
+        <strong>Example</strong>: Bitcoin is a popular example of a
+        cryptocurrency. It allows peer-to-peer transactions without the need for
+        a central authority or government.
+      </p>
+
+      <h2>Decentralized Trading Platform</h2>
+      <p>
+        <strong>What is it?</strong> A Decentralized Trading Platform (DTP) is a
+        system that enables users to trade financial assets directly without the
+        need for intermediaries like brokers. It operates on a decentralized
+        network, providing more control and transparency to users.
+      </p>
+      <p>
+        <strong>How it works?</strong> DTPs use blockchain technology to
+        facilitate and verify transactions. Smart contracts, self-executing
+        contracts with the terms of the agreement directly written into code,
+        play a crucial role in automating and securing trades on these
+        platforms.
+      </p>
+    </>
+  );
 }
 
 export default Project;
