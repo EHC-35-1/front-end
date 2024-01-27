93c2b27d33f60165f7749018df5e0506d89c15bf
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2024-01-27 14:32:46 +0700

    modified files: core comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
 import { Route, Routes, Link } from "react-router-dom";
 
 import { Home, About, Project } from "./pages";
-import "./App.css";
 
 function App() {
-    return (
-        <>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About Us</Link>
-                    </li>
-                    <li>
-                        <Link to="/project">Our Project</Link>
-                    </li>
-                </ul>
-            </nav>
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/project">Our Project</Link>
+          </li>
+        </ul>
+      </nav>
 
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/about" element={<About />}></Route>
-                <Route path="/project" element={<Project />}></Route>
-            </Routes>
-        </>
-    );
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/project" element={<Project />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
