38e3122438126d60e8a0a7c02fab244a978090e2
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2024-01-29 14:20:34 +0700

    modified header

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { FaCog } from "react-icons/fa"; // Import the settings icon from react-icons
 import { useTheme } from "../ThemeContext";
 
 // eslint-disable-next-line react/prop-types
@@ -12,27 +11,26 @@ function Header() {
   return (
     <nav className={`navbar navbar-expand-lg ${themeClass}`}>
       <div className="container">
-        {/* Settings Icon Button */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#settingsCollapse"
-          aria-controls="settingsCollapse"
-          aria-expanded="false"
-          aria-label="Toggle settings"
-        >
-          <FaCog className="navbar-toggler-icon" />
-        </button>
-
-        {/* Placeholder Logo */}
         <Link className="navbar-brand" to="/">
           Your Logo
         </Link>
 
+        {/* Settings Icon Button for Smaller Screens */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
         {/* Navigation Links */}
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link" to="/">
                 Home
@@ -48,18 +46,9 @@ function Header() {
                 Our Project
               </Link>
             </li>
-          </ul>
-        </div>
-
-        {/* Settings Collapse */}
-        <div className="collapse navbar-collapse" id="settingsCollapse">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <button className="btn btn-link nav-link" onClick={toggleTheme}>
-                Toggle Theme
-              </button>
+            <li className="nav-item" onClick={toggleTheme}>
+              <button className="nav-link">Toggle Theme</button>
             </li>
-            {/* Add other settings here, such as profile, etc. */}
           </ul>
         </div>
       </div>
