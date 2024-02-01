2bd51233a47ca668293ed03e10bc69cc1229fc5c
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2024-02-01 23:02:34 +0700

    modified route order

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,22 +1,22 @@
 import { FullScreen, Standard } from "./layouts";
 import {
-    Home,
-    About,
-    Project,
-    NotFound,
-    Trade,
-    Register,
-    Login,
+  Home,
+  About,
+  Project,
+  NotFound,
+  Trade,
+  Register,
+  Login,
 } from "./pages";
 
 export const publicRoutes = [
-    { path: "/", component: Home, layout: Standard },
-    { path: "/about", component: About, layout: Standard },
-    { path: "/project", component: Project, layout: Standard },
-    { path: "/trade", component: Trade, layout: Standard },
-    { path: "/login", component: Login, layout: FullScreen },
-    { path: "/*", component: NotFound, layout: Standard },
-    { path: "/register", component: Register, layout: Standard },
+  { path: "/", component: Home, layout: Standard },
+  { path: "/about", component: About, layout: Standard },
+  { path: "/project", component: Project, layout: Standard },
+  { path: "/trade", component: Trade, layout: Standard },
+  { path: "/login", component: Login, layout: FullScreen },
+  { path: "/register", component: Register, layout: Standard },
+  { path: "/*", component: NotFound, layout: Standard },
 ];
 
 export const privateRoutes = [];
