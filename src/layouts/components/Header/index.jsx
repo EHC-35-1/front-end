e697317dd8dc413914a87bc4b1616494c6d1a45b
baotan1909 <baotan1909@gmail.com>
2024-02-04 13:24:57 +0700

    Update logo in header

--- src/layouts/components/Header/index.jsx
+++ src/layouts/components/Header/index.jsx
@@ -3,57 +3,61 @@ import { useTheme } from "../../ThemeContext";
 
 // eslint-disable-next-line react/prop-types
 function Header() {
-    const { isDarkTheme, toggleTheme } = useTheme();
-    const themeClass = isDarkTheme
-        ? "navbar-dark bg-dark"
-        : "navbar-light bg-light";
+  const { isDarkTheme, toggleTheme } = useTheme();
+  const themeClass = isDarkTheme
+    ? "navbar-dark bg-dark"
+    : "navbar-light bg-light";
 
-    return (
-        <nav className={`navbar navbar-expand-lg ${themeClass}`}>
-            <div className="container">
-                <Link className="navbar-brand" to="/">
-                    Your Logo
-                </Link>
+  return (
+    <nav className={`navbar navbar-expand-lg ${themeClass}`}>
+      <div className="container">
+        <Link
+          className="navbar-brand fw-bold fs-1"
+          to="/"
+          style={{ color: "#169bfa" }}
+        >
+          Methaptamin
+        </Link>
 
-                {/* Settings Icon Button for Smaller Screens */}
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+        {/* Settings Icon Button for Smaller Screens */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                {/* Navigation Links */}
-                <div className="collapse navbar-collapse" id="navbarCollapse">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/about">
-                                About Us
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/project">
-                                Our Project
-                            </Link>
-                        </li>
-                        <li className="nav-item" onClick={toggleTheme}>
-                            <button className="nav-link">Toggle Theme</button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+        {/* Navigation Links */}
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/project">
+                Our Project
+              </Link>
+            </li>
+            <li className="nav-item" onClick={toggleTheme}>
+              <button className="nav-link">Toggle Theme</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
