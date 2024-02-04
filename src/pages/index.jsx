f6722579130f43fc0cfce1964ed3b9d763de043d
LeQuangHai204 <lequanghai.private@gmail.com>
2024-02-04 21:51:16 +0700

    Remove 'about us' page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 export { default as Home } from "./Home";
-export { default as About } from "./About";
 export { default as Project } from "./Project";
 export { default as NotFound } from "./NotFound";
 export { default as Trade } from "./Trade";
