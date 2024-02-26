f0e9f4cda1584d73d23825fd8d173ee286097d5d
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2024-02-26 16:02:52 +0700

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import { Route, Routes } from "react-router-dom";
-
+// team EHC 35.1
 import { publicRoutes } from "./routes";
 
 function App() {
-    return (
-        <Routes>
-            {/* To add to page, go to src/routes.jsx */}
-            {publicRoutes.map((route, index) => {
-                const Layout = route.layout;
-                const Page = route.component;
+  return (
+    <Routes>
+      {/* To add to page, go to src/routes.jsx */}
+      {publicRoutes.map((route, index) => {
+        const Layout = route.layout;
+        const Page = route.component;
 
-                return (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        element={
-                            <Layout>
-                                <Page />
-                            </Layout>
-                        }
-                    />
-                );
-            })}
-        </Routes>
-    );
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            element={
+              <Layout>
+                <Page />
+              </Layout>
+            }
+          />
+        );
+      })}
+    </Routes>
+  );
 }
 
 export default App;
