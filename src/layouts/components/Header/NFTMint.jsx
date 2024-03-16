32eea709691d6772f918a7196b0f30cf073b40dd
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2024-03-17 01:41:51 +0700

    added wallet connection

--- src/layouts/components/Header/NFTMint.jsx
+++ src/layouts/components/Header/NFTMint.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { ethers } from "ethers";
 
 function NFTMintButton() {
+  let address, provider;
+
+  const [isConnected, toggleConnected] = useState(0);
+
+  function setAddress(ethaddy) {
+    address = ethaddy;
+    if (address != null) {
+      toggleConnected(!isConnected);
+    }
+    console.log("Account:", address);
+    alert("Connected: " + address);
+  }
+
+  function handleButtonClick() {
+    if (!isConnected) {
+      connectWallet();
+    } else {
+      mintNFT();
+    }
+  }
+
+  function mintNFT() {}
+
+  async function connectWallet() {
+    provider = new ethers.BrowserProvider(window.ethereum);
+    // Prompt user for account connections
+    const res = await provider.send("eth_requestAccounts", []);
+    signer = provider.getSigner();
+    setAddress(res);
+  }
+
   return (
-    <button className="nav-link">
-      Mint NFT
-    </button>
+    <div
+      id="nftButton"
+      className="navbarBoxButton button-link"
+      onClick={handleButtonClick}
+    >
+      {isConnected ? "MINT NOW" : "CONNECT WALLET"}
+    </div>
   );
 }
 
